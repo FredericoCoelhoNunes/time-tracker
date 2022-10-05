c705b1ddb4b22f2588b0f1b1f33f6f44f9a819af
Fred <fred@DESKTOP-V8NLQLK.localdomain>
2022-10-05 16:53:51 +0100

    Added a very simple frontend to create and interact with multiple stopwatches

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,58 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <div class="main-window">
+      <div class="control-panel">
+          <p>This is the control panel</p>
+      </div>
+      <div class="stopwatch-section">
+        <div class="add-stopwatch-container">
+          <button type="button" @click="addStopwatch">Add Stopwatch</button>
+        </div>
+        <div class="stopwatch-list">
+          <my-stopwatch
+          v-for="(stopwatch, index) in stopwatches"
+          :key="stopwatch.uuid"
+          @saveStopwatch="(data) => saveStopwatch(index, data)"
+          @deleteStopwatch="deleteStopwatch(index)"
+          />
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
+<style scoped src='./assets/styles/app-styles.css'/>
+
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MyStopwatch from "./components/MyStopwatch.vue";
+  
+let uuid = 1;  // an ID for the stopwatches
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    MyStopwatch,
+  },
+  data() {
+    return {
+      stopwatches: [],
+    };
+  },
+  methods: {
+    addStopwatch() {
+      this.stopwatches.push({"uuid": uuid});
+      uuid += 1;
+    },
+    saveStopwatch(index, stopwatchData) {
+      this.saveToStorage(stopwatchData);
+      this.stopwatches.splice(index, 1);
+    },
+    saveToStorage(stopwatchData) {
+      console.log(stopwatchData);
+    },
+    deleteStopwatch(index) {
+      this.stopwatches.splice(index, 1);
+    }
+  },
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/MyStopwatch.vue
@@ -0,0 +1,121 @@
+<template>
+  <div class="my-stopwatch-container">
+    <div class="my-stopwatch-body bordered-section rounded">
+      <div class="my-stopwatch-task">
+        <div class="my-stopwatch-text-div">
+          <span contenteditable @input="onInput" class="my-stopwatch-text heading">{{ task }}</span>
+        </div>
+        <div class="my-stopwatch-trash-div">
+          <font-awesome-icon icon="fa-solid fa-trash" @click="deleteStopwatch"/>
+        </div>
+      </div>
+      <div class="my-stopwatch-clock bordered-section">
+        <span class="my-stopwatch-clock-text heading">{{ timeString }}</span>
+      </div>
+      <div class="my-stopwatch-buttons">
+        <div class="my-stopwatch-start-button-div">
+          <button class="my-stopwatch-start-button button" @click="startStopwatch">Start</button>
+        </div>
+        <div class="my-stopwatch-stop-button-div">
+          <button class="my-stopwatch-stop-button button" @click="stopStopwatch">Stop</button>
+        </div>
+        <div class="my-stopwatch-save-button-div">
+          <button class="my-stopwatch-save-button button" @click="saveStopwatch">Save</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped src='../assets/styles/my-stopwatch-styles.css'/>
+
+<script>
+// Adding a fontawesome icon to this component! (the trash can)
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+/* import specific icons */
+import { faTrash } from '@fortawesome/free-solid-svg-icons'
+
+/* add icons to the library */
+library.add(faTrash)
+
+// Stopwatch module
+const Stopwatch = require('statman-stopwatch');
+
+function msToTime(duration) {
+  // Converts milliseconds to a HH:MM:SS string
+  var milliseconds = Math.floor((duration % 1000) / 100),
+    seconds = Math.floor((duration / 1000) % 60),
+    minutes = Math.floor((duration / (1000 * 60)) % 60),
+    hours = Math.floor((duration / (1000 * 60 * 60)) % 24);
+
+  hours = (hours < 10) ? "0" + hours : hours;
+  minutes = (minutes < 10) ? "0" + minutes : minutes;
+  seconds = (seconds < 10) ? "0" + seconds : seconds;
+
+  return hours + ":" + minutes + ":" + seconds + "." + milliseconds;
+}
+
+export default {
+  name: 'MyStopwatch',
+  components: {
+    FontAwesomeIcon
+  },
+  data() {
+    return {
+      task: 'New Task',
+      timeString: '00:00:00',
+      stopwatchState: "Paused"
+    }
+  },
+  emits: ['saveStopwatch', 'deleteStopwatch'],
+  methods: {
+    onInput(e) {
+      this.task = e.target.innerText;
+    },
+    startStopwatch() {
+      if (!this.stopwatch) {
+        console.log('started stopwatch with id '+this.uuid)
+        this.stopwatch = new Stopwatch();
+        this.stopwatch.start();
+        this.updateIntervalID = setInterval(this.updateDisplay, 100);
+      } else if (this.stopwatchState == "Paused") {
+        this.stopwatch.reset();
+        this.stopwatch.setStartTimeDelta(this.curr_time);
+        this.stopwatch.start();
+      }
+      this.stopwatchState = "Running";
+
+    },
+    stopStopwatch() {
+      if (this.stopwatchState == "Running") {
+        this.curr_time = this.stopwatch.stop();
+        this.stopwatchState = "Paused";
+      }
+    },
+    saveStopwatch() {
+      this.stopStopwatch();
+      if (this.updateIntervalID) {
+        clearInterval(this.updateIntervalID);  
+      }
+      this.stopwatchState = 'Locked';
+      this.$emit('saveStopwatch', this.$data);
+    },
+    deleteStopwatch() {
+      // TODO: dry! (repeated block of code)
+      this.stopStopwatch();
+      if (this.updateIntervalID) {
+        clearInterval(this.updateIntervalID);  
+      }
+      this.stopwatchState = 'Locked';
+      this.$emit('deleteStopwatch');
+    },
+    updateDisplay() {
+      this.timeString = msToTime(this.stopwatch.read());
+    }
+
+  },
+}
+</script>
